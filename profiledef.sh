d816ec815ff6c519a0027b5aa363e56de3cea516
kevadesu <kevadesu@asuslap.top>
2025-12-19 18:29:27 +0100

    rework

--- profiledef.sh
+++ profiledef.sh
@@ -4,7 +4,7 @@
 iso_name="arctineos"
 iso_label="ARCTINE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="
-Icycoide Development Laboratories  <https://icycoide.github.io>"
+Arctine Laboratories <https://arctine.rootsource.cc>"
 iso_application="Arctine OS Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
