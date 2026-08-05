90c4119b9cbf65a4ba6a806b065c7493fcdc7ce3
kevadesu <kevadesu@asuslap.top>
2026-08-05 03:13:14 +0200

    Add GNOME Initial Setup, actual locales and helpful information during setup.sh

--- airootfs/Arctine/GumpackNG/setup.sh
+++ airootfs/Arctine/GumpackNG/setup.sh
@@ -2,8 +2,54 @@
 
 # This script is a big mess, I might clean it up later on...
 
+function scriptcolors() {
+    # Reset
+    export Color_Off='\033[0m'       # Text Reset
+
+    # Regular Colors
+ 
+    # Bold
+    export BRed='\033[1;31m'         # Red
+    export BYellow='\033[1;33m'      # Yellow
+    export BBlue='\033[1;34m'        # Blue
+    export BCyan='\033[1;36m'        # Cyan
+ 
+    echo -e "${BBlue}[i] The colour variables have been set.${Color_Off}"
+}
+
+limsg() {
+    # LiMSG, as seen in the TyrianOS builder.
+    case $1 in
+        s)
+            lmsg_one="STAGE"
+        ;;
+
+    esac
+    lmsg_two=$2
+    case $3 in
+        i)
+            lmsg_three="${BBlue}INFO"
+        ;;
+        w)
+            lmsg_three="${BYellow}WARN"
+        ;;
+        e)            
+            lmsg_three="${BRed}ERRR"
+        ;;
+        *)
+            lmsg_three=$3
+        ;;
+    esac
+    echo -e "[ArctineOS Installer(LiMSG)/${BCyan}${lmsg_one} ${lmsg_two}${Color_Off}/${lmsg_three}${Color_Off}]: $4"
+}
+
+scriptcolors
+
+limsg s 1 i "Populating keyring..."
 pacman-key --init
 pacman-key --populate archlinux
+
+limsg s 1 i "Installing dracut..."
 pacman -S dracut --noconfirm
 
 git config --global --add safe.directory /OS
@@ -11,39 +57,54 @@ git config --global --add safe.directory /OS
 # shellcheck disable=SC2164
 pushd /OS/arctine-pkg
     chown nobody:nobody -Rv .
+    limsg s 1 i "Making Milanium..."
     sudo -u nobody makepkg -sr
+    limsg s 1 i "Installing Milanium..."
     pacman -Uv ./milanium-*.pkg.tar.zst --noconfirm
 # shellcheck disable=SC2164
 popd
 
+limsg s 1 i "Syncing clock..."
 hwclock --systohc
 
+limsg s 1 i "Running filesystem override..."
 /Arctine/Scripts/hookhelper filesystem
 
+limsg s 1 i "Enabling NetworkManager..."
 systemctl enable NetworkManager
+limsg s 1 i "Enabling GNOME Display Manager..."
 systemctl enable gdm
 
+limsg s 1 i "Removing now obsolete mkinitcpio..."
 pacman -R mkinitcpio mkinitcpio-archiso --noconfirm
 
+limsg s 1 i "Force generate image kernel..."
 dracut --force
 
 if cat /sys/firmware/efi/fw_platform_size; then
+    limsg s 1 i "Installing GRUB for EFI..."
     grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 else
     grub-install --target=i386-pc "$(cat /bootpart.txt)"
 fi
 
+limsg s 1 i "Generating GRUB configuration..."
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo '%wheel ALL=(ALL:ALL) ALL' > /etc/sudoers.d/10-wheel
 chmod 440 /etc/sudoers.d/10-wheel
 visudo -c
 
-read -rp "Enter username for new user: " Username
-useradd -m -G wheel "$Username"
+limsg s 1 i "Generating locales (this might take a while!)..."
+locale-gen
 
-while ! passwd "$Username"; do
-    echo "Try again"
-done
+# This will be done by the initial setup in the future.
+
+#read -rp "Enter username for new user: " Username
+#useradd -m -G wheel "$Username"
+#
+#while ! passwd "$Username"; do
+#    echo "Try again"
+#done
 
 echo "Exiting back to host"
